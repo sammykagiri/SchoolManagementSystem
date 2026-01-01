3ee1c495c2a02e0fdeb8051bbc349e948a994546
Sammy Kagiri <sammykagiri@gmail.com>
2026-01-02 02:11:51 +0300

    Logo Storage fix14

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -21,6 +21,28 @@ class RailwayStorage(S3Boto3Storage):
         self.location = getattr(settings, 'AWS_LOCATION', 'media')
         super().__init__(*args, **kwargs)
     
+    def _get_s3_client(self):
+        """
+        Get or create an S3 client for generating presigned URLs
+        The connection property returns a Resource, but we need a Client
+        """
+        if not hasattr(self, '_s3_client'):
+            import boto3
+            
+            endpoint_url = getattr(settings, 'AWS_S3_ENDPOINT_URL', None)
+            access_key = getattr(settings, 'AWS_ACCESS_KEY_ID', None)
+            secret_key = getattr(settings, 'AWS_SECRET_ACCESS_KEY', None)
+            region_name = getattr(settings, 'AWS_S3_REGION_NAME', 'us-east-1')
+            
+            self._s3_client = boto3.client(
+                's3',
+                aws_access_key_id=access_key,
+                aws_secret_access_key=secret_key,
+                endpoint_url=endpoint_url.rstrip('/') if endpoint_url else None,
+                region_name=region_name,
+            )
+        return self._s3_client
+    
     def url(self, name):
         """
         Override url method to generate presigned URLs for Railway storage
@@ -32,7 +54,8 @@ class RailwayStorage(S3Boto3Storage):
         # Generate presigned URL (expires in 1 year = 31536000 seconds)
         # This allows direct access to the file without making the bucket public
         try:
-            url = self.connection.generate_presigned_url(
+            client = self._get_s3_client()
+            url = client.generate_presigned_url(
                 'get_object',
                 Params={
                     'Bucket': self.bucket_name,
